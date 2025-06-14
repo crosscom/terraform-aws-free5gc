8d76d5977e126d344549600ed1185e877a411af7
Young Jung <40037406+crosscom@users.noreply.github.com>
2025-06-13 22:24:46 -0500

    Add EKS module outputs

---
+++ modules/eks/outputs.tf
@@ -0,0 +1,24 @@
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = aws_eks_cluster.this.id
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = aws_eks_cluster.this.arn
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+output "node_groups" {
+  description = "Map of EKS node groups"
+  value       = aws_eks_node_group.this
+}
